1a03c5ef30f97d433415169d82d2da3ceed1cbc9
Dimidroll66 <dima.shved.06@gmail.com>
2025-08-28 16:30:51 +0300

    Header and Loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Header } from "./widgets/Header/Header"
+
 function App() {
 
   return (
     <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+    )
 }
 
 export default App
