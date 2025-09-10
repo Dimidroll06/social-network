f3d1e35884b580ece837b805ccbe96b70aac61f5
Dimidroll66 <dima.shved.06@gmail.com>
2025-09-10 03:17:53 +0300

    Make header fixed height; Make margin for all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Header } from "./widgets/Header/Header"
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Header } from './widgets/Header/Header';
 
 function App() {
-
   return (
     <>
       <Header />
+      <div className="mt-16"></div>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </>
-    )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/widgets/Header/Header.tsx
+++ frontend/src/widgets/Header/Header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-white/50 border-b border-gray-200">
+      <header className="fixed top-0 left-0 right-0 z-50 h-16 backdrop-blur-md bg-white/50 border-b border-gray-200">
         <div className="container mx-auto px-4 py-3 flex items-center justify-between">
           {handleMobileSearch || <Logo />}
           <Search
